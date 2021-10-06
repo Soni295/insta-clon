532c165c4d588d8f0326dabe088ad75abed599c6
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-10-06 08:26:06 -0300

    feat(backend): add environment variables
      * for database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-import bodyParser from 'body-parser'
-
 import { route } from './route/index.js'
+
 
 const app = express()
 const port = '3001'
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.use(cors())
 app.use(route)
 
