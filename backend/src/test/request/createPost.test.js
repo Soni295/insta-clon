a34d578ce15bc75cd78a928e148fb3bcbc2d452f
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-10-16 23:32:29 -0300

    feat(backend): add test for post
      * add test in case successfully and error
        for post with token

--- backend/src/test/request/createPost.test.js
+++ backend/src/test/request/createPost.test.js
@@ -1,30 +1,54 @@
 import supertest from 'supertest'
 import app from '../../app'
 
-const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlciI6ImFydHVybyIsImlhdCI6MTYzNDI2MTAxMn0._oLgA2_4h83BnnIIWVttqRPzRuaFSQGTnS7-C9T2xl4'
-
-let base = {'Authorization': token, 'Content-Type': 'application/json'}
-
 const request = supertest(app)
 
 describe('Post /Post (createpost)', () => {
-  it('responds with LogIn successfully, user and token', done => {
+  it('responds with successfully posted msg', done => {
+    let token = 'Bearer '
+    token += 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'
+    token += 'eyJpZCI6MSwidXNlciI6ImFydHVybyIsImlhdCI6MTYzNDI2MTAxMn0.'
+    token += '_oLgA2_4h83BnnIIWVttqRPzRuaFSQGTnS7-C9T2xl4'
+
+    const base = {'Authorization': token, 'Content-Type': 'application/json'}
+
     request
       .post('/Post')
       .set(base)
-//      .send({user: 'arturo', password: '123456'})
-//      .expect('Content-Type', /json/)
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        const {token, ...data} = res.body
-        const jsonData = JSON.stringify(data)
-        const expect ='{"msg":"LogIn successfully","user":"arturo"}'
+        const { msg } = res.body
 
-        if(expect !== jsonData || !token) {
-          return done(err)
+        if(msg !== 'Post created Successfully'){
+          return done('unexpect JSON response')
         }
 
+        return done()
+      })
+  })
+})
+
+describe('Post /Post (createpost)', () => {
+  it(`responds with error 401 with msg 'token is missing or invalid'`, done => {
+    let badtoken = 'Bearer '
+    badtoken += 'eaJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'
+    badtoken += 'eyJpZCI6MSwidXNlciI6ImFydHVybyIsImlhdCI6MTYzNDI2MTAxMn0.'
+    badtoken += '_oLgA2_4h83BnnIIWVttqRPzRuaFSQGTnS7-C9T2xl4'
+
+    const base = {'Authorization': badtoken, 'Content-Type': 'application/json'}
+
+    request
+      .post('/Post')
+      .set(base)
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { msg } = res.body
+
+        if(msg !== 'token is missing or invalid'){
+          return done('unexpect JSON response')
+        }
         return done()
       })
   })
