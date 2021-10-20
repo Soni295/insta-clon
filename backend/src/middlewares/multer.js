f070c2781ff9cac79de7c477da6d978a5426988b
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-10-20 00:29:57 -0300

    feat(backend): set multer

--- backend/src/middlewares/multer.js
+++ backend/src/middlewares/multer.js
@@ -1,3 +1,17 @@
 import multer from 'multer'
+import { join, dirname }from 'path'
+import { fileURLToPath } from 'url'
 
-export const uploadImg = multer({dest: './public/uploads'})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const destination = join(__dirname, '../public/uploads')
+
+const storage = multer.diskStorage({
+  destination,
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+export const uploadImg = multer({storage})
